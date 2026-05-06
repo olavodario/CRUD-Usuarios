690eb8bc3f45a60f32663a68676feaf3b76895fd
Olavo Dario <olavoribgodario@gmail.com>
2026-05-05 21:52:57 -0300

    feat: Adição do sistema de Login

--- src/ETEC/Main.java
+++ src/ETEC/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    new FrmMain().setVisible(true);
+    new FrmLogin().setVisible(true);
 }
     
 }
